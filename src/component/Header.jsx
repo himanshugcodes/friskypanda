97e6c005a6234c23fc4b7ba013bd63a591551c33
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Air.local>
2024-09-24 00:13:25 +0530

    test

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,35 @@
 import React from "react";
-
+import Image from "next/image";
+import Link from "next/link";
 const header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="logo">
+                <Image
+                  src="/assets/images/logo.png"
+                  width={100}
+                  height={120}
+                  alt="Frisky Panda"
+                />
+              </div>
+            </div>
+            <div className="col-lg-8">
+              <nav>
+                <ul>
+                  <li>
+                    <Link href="/">Home</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 

9c3190b801a26a24415b29722fc5f0139dd1e7f0
Himanshu Gupta <himanshu.gupta94150@gmail.com>
2024-09-24 00:55:24 +0530

    test ci cd

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,24 +4,47 @@ import Link from "next/link";
 const header = () => {
   return (
     <>
-      <header>
+      <header className="">
         <div className="container">
-          <div className="row">
-            <div className="col-lg-4">
+          <div className="row justify-content-between align-items-center">
+            <div className="col-lg-3">
               <div className="logo">
-                <Image
-                  src="/assets/images/logo.png"
-                  width={100}
-                  height={120}
-                  alt="Frisky Panda"
-                />
+                <Link href="/">
+                  <Image
+                    src="/assets/images/logo.png"
+                    width={100}
+                    height={120}
+                    alt="Frisky Panda"
+                  />
+                </Link>
               </div>
             </div>
-            <div className="col-lg-8">
-              <nav>
+            <div className="col-lg-9 d-flex justify-content-end">
+              <nav className="navigation">
                 <ul>
                   <li>
                     <Link href="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link href="/about">About Us</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Birthday Parties</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Events</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Cafe</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Gallery</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Membership</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Contact Us</Link>
                   </li>
                 </ul>
               </nav>

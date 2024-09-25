f53800e94d2526404060317f9d14c936031bb75b
Himanshu Gupta <himanshu.gupta94150@gmail.com>
2024-09-25 23:14:00 +0530

    loading file

---
+++ src/app/loading.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="mt-5">
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+      <h1>Loading</h1>
+    </div>
+  );
+};
+
+export default loading;
